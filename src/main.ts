5d0ddb61485c97172ac99c0e862954cfca71fb78
Maksim <sharkomaks@gmail.com>
2025-02-05 12:30:33 +0300

    add prisma and schema

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+	app.setGlobalPrefix('api');
+	app.use(cookieParser());
+	app.enableCors({
+		origin: ['http://localhost:3000'],
+		credentials: true,
+		exposedHeaders: 'set-cookie'
+	});
+	await app.listen(process.env.PORT ?? 5000);
 }
+
 bootstrap();
