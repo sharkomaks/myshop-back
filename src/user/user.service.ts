fb3a694925c803ed36e19216af339cbd4599ad4f
Maksim <sharkomaks@gmail.com>
2025-02-06 12:02:14 +0300

    add user

---
+++ src/user/user.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { hash } from 'argon2';
+import { AuthDto } from '../auth/dto/auth.dto';
+
+@Injectable()
+export class UserService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async getById(id: string) {
+		return this.prisma.user.findUnique({
+			where: {
+				id
+			},
+			include: {
+				stores: true,
+				favorites: true,
+				orders: true
+			}
+		});
+	}
+
+	async getByEmail(email: string) {
+		return this.prisma.user.findUnique({
+			where: {
+				email
+			},
+			include: {
+				stores: true,
+				favorites: true,
+				orders: true
+			}
+		});
+	}
+
+	async create(dto: AuthDto) {
+		return this.prisma.user.create({
+			data: {
+				...dto,
+				password: await hash(dto.password)
+			}
+		});
+	}
+}
