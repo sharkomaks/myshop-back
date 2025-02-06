5318ab31a55cd84d04d4e48c3811e0f87fbbb22f
Maksim <sharkomaks@gmail.com>
2025-02-06 12:00:23 +0300

    add auth

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class AuthDto {
+	@IsOptional()
+	@IsString()
+	name: string;
+
+	@IsString({
+		message: 'Почта обязательна'
+	})
+	@IsEmail()
+	email: string;
+
+	@MinLength(8, {
+		message: 'Пароль должен содержать не менее 8 символов!'
+	})
+	@IsString({
+		message: 'Пароль обязателен'
+	})
+	password: string;
+}
